3228b7883c44b2caf270d1c05ae33d9525acd836
olingo99 <engels.theo@skynet.be>
2023-12-02 11:53:26 +0100

    day2 easy

--- src/day2.rs
+++ src/day2.rs
@@ -1,60 +1,81 @@
-use std::collections::HashMap;
-use std::io::{BufRead, BufReader};
-use std::fs::File;
-
 use crate::input;
+use std::collections::HashMap;
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref MAP: HashMap<&'static str, u32> = {
+        let mut m = HashMap::new();
+        m.insert("red", 12);
+        m.insert("green", 13);
+        m.insert("blue", 14);
+        m
+    };
+}
+
+// pub fn day2() -> input::Result<()> {
+//     let contents = input::load_day_file("day2.txt");
+//     let mut tot:u32 = 0;
+//     'outer :for line in contents.lines() {
+//         println!("{}", line);
+//         let mut split = line.split(":");
+//         let id:u32 = split.next().unwrap().split_whitespace().last().unwrap().parse()?;
+//         let game = split.next().unwrap();
+//         dbg!(id);
+//         dbg!(game);
+//         for set in game.split(";"){
+//             dbg!(set);
+//             for pull in set.split(","){
+//                 dbg!(pull);
+//                 let mut pull = pull.split_whitespace();
+//                 let nb:u32 = pull.next().unwrap().parse()?;
+//                 let color = pull.next().unwrap();
+//                 dbg!(nb);
+//                 dbg!(color);
+//                 if nb > *MAP.get(color).unwrap() {
+//                     continue 'outer;
+//                 }
+//             }
+//         }
+//         tot += id;
+//         dbg!(id);
+//         dbg!(tot);
+//     }
+//     dbg!(tot);
+//     Ok(())
+// }
+
+
 
 pub fn day2() -> input::Result<()> {
-    let mut score : u32 = 0;
-    let reader = BufReader::new(File::open("data/day2.txt").expect("Cannot open file.txt"));
-    let map : HashMap<String, u32> = HashMap::from([
-        ("A X".to_string(), 3),
-        ("A Y".to_string(), 4),
-        ("A Z".to_string(), 8),
-        ("B X".to_string(), 1),
-        ("B Y".to_string(), 5),
-        ("B Z".to_string(), 9),
-        ("C X".to_string(), 2),
-        ("C Y".to_string(), 6),
-        ("C Z".to_string(), 7),
-    ]);
-    for line in reader.lines() {
-
-        score += map.get(&line.unwrap()).unwrap();
+    let contents = input::load_day_file("day2.txt");
+    let mut tot:u32 = 0;
+    'outer :for line in contents.lines() {
+        let mut min_cubes: HashMap<String, u32> = HashMap::from([
+            ("red".to_string(), 0),
+            ("green".to_string(), 0),
+            ("blue".to_string(), 0),
+        ]);
+        println!("{}", line);
+        let mut split = line.split(":");
+        let id:u32 = split.next().unwrap().split_whitespace().last().unwrap().parse()?;
+        let game = split.next().unwrap();
+        dbg!(id);
+        dbg!(game);
+        for set in game.split(";"){
+            // dbg!(set);
+            for pull in set.split(","){
+                // dbg!(pull);
+                let mut pull = pull.split_whitespace();
+                let nb:u32 = pull.next().unwrap().parse()?;
+                let color = pull.next().unwrap();
+                if nb > *min_cubes.get(color).unwrap() {
+                    min_cubes.insert(color.to_string(), nb);
+                }
+            }
+        }
+        dbg!(&min_cubes);
+        let mult:u32 = min_cubes.values().product();
+        tot +=mult;
     }
-    dbg!(score);
-    print!("Hello");
+    dbg!(tot);
     Ok(())
-}
-
-fn translate(word: &str ) -> u32{
-    match word {
-        "X" => 0,
-        "Y" => 3,
-        "Z" => 6,
-        _ => 0,
-    }
-}
-
-pub fn exo1(){
-    let map : HashMap<String, u32> = HashMap::from([
-        ("A X".to_string(), 4),
-        ("A Y".to_string(), 8),
-        ("A Z".to_string(), 3),
-        ("B X".to_string(), 1),
-        ("B Y".to_string(), 5),
-        ("B Z".to_string(), 9),
-        ("C X".to_string(), 7),
-        ("C Y".to_string(), 2),
-        ("C Z".to_string(), 6),
-    ]);
-    let mut score : u32 = 0;
-    let reader = BufReader::new(File::open("../data/rpc.txt").expect("Cannot open file.txt"));
-
-    for line in reader.lines() {
-        //score += map.get(line.unwrap()).unwrap();
-        //dbg!(line.unwrap());
-        score += map.get(&line.unwrap()).unwrap();
-    }
-    dbg!(score);
 }
