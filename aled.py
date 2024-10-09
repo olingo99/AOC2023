33ab0775cd22baf5e0bf616ea7ef3d5f1d8bf6f8
Olingo <195367@ecam.be>
2024-10-09 18:47:37 +0200

    s

--- aled.py
+++ aled.py
@@ -1,18 +1,82 @@
-def eval(line):
-    hand, bid = line.split()
-    hand = hand.translate(str.maketrans('TJQKA', face))
-    best = max(type(hand.replace('0', r)) for r in hand)
-    return best, hand, int(bid)
+import re
+ll = [x for x in open('data/day19.txt').read().strip().split('\n\n')]
+workflow, parts = ll
 
-def type(hand):
-    return sorted(map(hand.count, hand), reverse=True)
+def ints(s):
+	return list(map(int, re.findall(r'\d+', s)))
 
-for face in 'ABCDE', 'A0CDE':
-    print(sum(rank * bid for rank, (*_, bid) in
-        enumerate(sorted(map(eval, open('data/day7.txt'))), start=1)))
-    # print((rank, bid) for rank, (*_, bid) in
-    #     enumerate(sorted(map(eval, open('data/day7.txt'))), start=1))
+parts = [ints(l) for l in parts.split("\n")]
+workflow = {l.split("{")[0]: l.split("{")[1][:-1] for l in workflow.split("\n")}
 
-    # with open('res.txt', 'w') as f:
-    #     for (rank, (*_, bid)) in enumerate(sorted(map(eval, open('data/day7.txt'))), start=1):
-    #         f.write(f"{bid},\n")
+def eval2(part, work):
+	w = workflow[work]
+	x, m, a, s = part
+	for it in w.split(","):
+		if it == "R":
+			return False
+		if it == "A":
+			return True
+		if ":" not in it:
+			return eval2(part, it)
+		cond = it.split(":")[0]
+		if eval(cond):
+			if it.split(":")[1] == "R":
+				return False
+			if it.split(":")[1] == "A":
+				return True
+			return eval2(part, it.split(":")[1])
+	raise Exception(w)
+
+p1 = 0
+
+for part in parts:
+	if eval2(part, 'in'):
+		p1 += sum(part)
+print(p1)
+
+
+def both(ch, gt, val, ranges):
+	ch = 'xmas'.index(ch)
+	ranges2 = []
+	for rng in ranges:
+		rng = list(rng)
+		lo, hi = rng[ch]
+		if gt:
+			lo = max(lo, val + 1)
+		else:
+			hi = min(hi, val - 1)
+		if lo > hi:
+			continue
+		rng[ch] = (lo, hi)
+		ranges2.append(tuple(rng))
+	return ranges2
+
+
+def acceptance_ranges_outer(work):
+	return acceptance_ranges_inner(workflow[work].split(","))
+
+def acceptance_ranges_inner(w):
+	it = w[0]
+	if it == "R":
+		return []
+	if it == "A":
+		return [((1, 4000), (1, 4000), (1, 4000), (1, 4000))]
+	if ":" not in it:
+		return acceptance_ranges_outer(it)
+	cond = it.split(":")[0]
+	gt = ">" in cond
+	ch = cond[0]
+	val = int(cond[2:])
+	val_inverted = val + 1 if gt else val - 1
+	if_cond_is_true = both(ch, gt, val, acceptance_ranges_inner([it.split(":")[1]]))
+	if_cond_is_false = both(ch, not gt, val_inverted, acceptance_ranges_inner(w[1:]))
+	return if_cond_is_true + if_cond_is_false
+
+p2 = 0
+for rng in acceptance_ranges_outer('in'):
+	v = 1
+	print(rng)
+	for lo, hi in rng:
+		v *= hi - lo + 1
+	p2 += v
+print(p2)
--- src/day19.rs
+++ src/day19.rs
@@ -1,13 +1,17 @@
 use crate::input;
 use std::collections::HashMap;
+use std::collections::VecDeque;
+use std::cmp::max;
+use std::cmp::min;
+
 
 
 pub fn day19() -> input::Result<()> {
     let contents = input::load_day_file("day19.txt");
 
-    let parsed_content = parse(contents);
+    let parsed_content = parse2(contents);
 
-    solve_part1(parsed_content.0, parsed_content.1);
+    solve_part2(parsed_content);
     Ok(())
 }
 
@@ -142,5 +146,198 @@ fn solve_part1(
     }
 
     dbg!(total);
+}
+#[derive(Debug, PartialEq, Eq)]
+enum Cond{
+    bigger,
+    smaller,
+    None
+}
+#[derive(Debug)]
+struct Rule{
+    target: char,
+    cond: Cond,
+    lim: isize,
+    res: String,
+}
+
+
+#[derive(Debug, Clone)]
+struct Range{
+    x:(isize,isize),
+    m:(isize,isize),
+    a:(isize,isize),
+    s:(isize,isize),
+    loc: String,
+    loc_hist:Vec<String>
+}
+
+impl Range {
+    // Method to get an immutable reference to the range
+    fn get_range(&self, c: char) -> Option<&(isize, isize)> {
+        match c {
+            'x' => Some(&self.x),
+            'm' => Some(&self.m),
+            'a' => Some(&self.a),
+            's' => Some(&self.s),
+            _ => None,
+        }
+    }
+
+    // Method to get a mutable reference to the range
+    fn get_range_mut(&mut self, c: char) -> Option<&mut (isize, isize)> {
+        match c {
+            'x' => Some(&mut self.x),
+            'm' => Some(&mut self.m),
+            'a' => Some(&mut self.a),
+            's' => Some(&mut self.s),
+            _ => None,
+        }
+    }
+}
+
+fn parse2(content: String)->HashMap<String, Vec<Rule>>{
+        
+    let mut sections = content.split("\r\n\r\n");
+    let rules = sections.next().unwrap_or("");
+
+    let mut rules_map:HashMap<String, Vec<Rule>> = HashMap::new();
+
+
+    for rule in rules.lines() {
+        let mut rules_vec :Vec<Rule> = Vec::new();
+
+        if let Some(pos) = rule.find('{') {
+            let key = rule[..pos].to_string();
+            let rulelist = rule[pos..].trim_matches(|c| c == '{' || c == '}').to_string();
+
+            for elem in rulelist.split(','){
+                
+                let result: Vec<&str> = elem.split(':').collect();
+                let count = result.len();
+
+
+                if count == 1 {
+                    let r: String = result.last().unwrap().to_string();
+
+                    rules_vec.push(Rule { target: 'o', cond: Cond::None, lim: 0, res: r });
+                }
+
+                else{
+                    let elem = result[0].to_string();
+                    let r = result[1].to_string();
+                    if elem.contains('>'){
+
+                        let mut split = elem.split('>');
+                        let target_str = split.next().unwrap().to_string();
+                        let target = target_str.chars().next().unwrap();
+                        let lim = split.next().unwrap().parse::<isize>().unwrap();
+
+                        rules_vec.push(
+                            Rule{
+                                target: target,
+                                cond: Cond::bigger,
+                                lim,
+                                res: r
+                            }
+                        );
+                    }
+
+                    else if elem.contains('<'){
+                        let mut split = elem.split('<');
+                        let target_str = split.next().unwrap().to_string();
+                        let target = target_str.chars().next().unwrap();
+                        let lim = split.next().unwrap().parse::<isize>().unwrap();
+
+                        rules_vec.push(
+                            Rule{
+                                target: target,
+                                cond: Cond::smaller,
+                                lim,
+                                res: r
+                            }
+                        );
+
+                    }
+                }
+            }
+
+            rules_map.insert(key, rules_vec);
+        }
+    }
+
+    rules_map
+
+}
+
+fn solve_part2(rules: HashMap<String, Vec<Rule>>){
+    let mut ranges: VecDeque<Range> = VecDeque::new();
+    ranges.push_back(Range { x: (1,4000), m: (1,4000), a: (1,4000), s: (1,4000), loc: "in".to_string(), loc_hist:vec!["in".to_string()] });
+
+    let mut done:Vec<Range> = Vec::new();
+
+    while !ranges.is_empty(){
+        // dbg!(&ranges);
+
+        let mut range = ranges.pop_front().unwrap();
+
+        if range.loc == "A"{
+            done.push(range);
+            continue;
+        }
+        if range.loc == "R"{
+            continue;
+        }
+
+
+        for rule in rules.get(&range.loc).unwrap(){
+
+            if rule.cond == Cond::bigger && range.get_range(rule.target).unwrap().1 >= rule.lim{
+                let mut new_range = range.clone();
+                // dbg!(&new_range);
+                new_range.get_range_mut(rule.target).unwrap().0 = max(rule.lim , range.get_range(rule.target).unwrap().0)+1;
+                range.get_range_mut(rule.target).unwrap().1 = min(rule.lim, range.get_range(rule.target).unwrap().1);
+                
+                // dbg!(&new_range);
+                new_range.loc_hist.push(rule.res.clone());
+                new_range.loc = rule.res.clone();
+                ranges.push_back(new_range);
+            }
+
+            else if rule.cond == Cond::smaller && range.get_range(rule.target).unwrap().0 <= rule.lim {
+                let mut new_range = range.clone();
+            
+                new_range.get_range_mut(rule.target).unwrap().1 = min(rule.lim, range.get_range(rule.target).unwrap().1)-1;
+                range.get_range_mut(rule.target).unwrap().0 = max(rule.lim , range.get_range(rule.target).unwrap().0);
+
+                new_range.loc = rule.res.clone();
+                new_range.loc_hist.push(rule.res.clone());
+                ranges.push_back(new_range);
+            }
+
+            else if rule.cond == Cond::None{
+                let mut new_range = range.clone();
+                new_range.loc = rule.res.clone();
+                new_range.loc_hist.push(rule.res.clone());
+                ranges.push_back(new_range);
+            }
+
+        }
+    }
+
+    // dbg!(&done.len());
+    // dbg!(&done);
+
+
+    let mut tot = 0;
+    for range in &done{
+        tot+= (range.x.1-range.x.0 +1) * (range.m.1-range.m.0 +1) * (range.a.1-range.a.0 +1) * (range.s.1-range.s.0+1) ;
+    }
+
+    // for range in &done{
+    //     println!("{:?}{:?}{:?}{:?}", range.x, range.m, range.a, range.s)
+    // }
+
+    dbg!(tot);
 }
 
